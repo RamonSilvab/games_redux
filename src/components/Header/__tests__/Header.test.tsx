e3d676cd201dd3cd6fc8a40a71214e44d15ee8db
RamonSilvaB <ramonsilvab05@gmail.com>
2024-08-02 10:12:23 -0300

    configurando a store para testes com jest

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -1,16 +1,41 @@
-import { Provider } from 'react-redux'
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import Header from '..'
-import { store } from '../../../store'
+import { rederizaComProvider } from '../../../utils/tests'
 
 describe('Teste para o componente header', () => {
   test('Deve renderizar corretamente', () => {
-    const { debug } = render(
-      <Provider store={store}>
-        <Header />
-      </Provider>
-    )
-    debug()
+    rederizaComProvider(<Header />)
     expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+  })
+
+  test('Deve renderizar com 2 itens no carrinho', () => {
+    rederizaComProvider(<Header />, {
+      preloadedState: {
+        carrinho: {
+          itens: [
+            {
+              id: 1,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windowns'],
+              preco: 149.9,
+              precoAntigo: 199.9,
+              titulo: 'Elden Ring'
+            },
+            {
+              id: 2,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windowns', 'PS5', 'Xbox Series S/X'],
+              preco: 199.9,
+              precoAntigo: 249.9,
+              titulo: 'Hogwarts Legacy'
+            }
+          ]
+        }
+      }
+    })
+
+    expect(screen.getByTestId('qtd-carrinho').innerHTML).toContain('2 itens')
   })
 })
--- src/utils/tests.tsx
+++ src/utils/tests.tsx
@@ -1,17 +1,34 @@
-import carrinho from '../store/reducers/carrinho'
+import { PreloadedState } from '@reduxjs/toolkit'
+import { RenderOptions, render } from '@testing-library/react'
 
-interface Pessoa {
-  nome: string
-  idade: number
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+
+import { AppStore, RootState, configuraStore } from '../store'
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: AppStore
 }
-
-interface ExtendedRenderOptions extends Omit<Pessoa, 'idade'>
 
 export function rederizaComProvider(
   elemento: React.ReactElement,
   {
     preloadedState = {},
-    store,
+    store = configuraStore(preloadedState),
     ...opcoesAdicionais
   }: ExtendedRenderOptions = {}
-)
+) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function Encapsulador({ children }: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+
+  return {
+    store,
+    ...render(elemento, {
+      wrapper: Encapsulador,
+      ...opcoesAdicionais
+    })
+  }
+}
